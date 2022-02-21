e4bbc6c9b2eea5f86e2f3611e919cdda0841a93b
BCNJ <jeremy.beaucousin@cnieg.fr>
2022-02-21 11:04:31 +0100

    Supression composants par défaut

--- src/lib/lhyfe-nav.module.ts
+++ src/lib/lhyfe-nav.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { LhyfeNavComponent } from './lhyfe-nav.component';
 
 
 
 @NgModule({
   declarations: [
-    LhyfeNavComponent
   ],
   imports: [
   ],
   exports: [
-    LhyfeNavComponent
   ]
 })
 export class LhyfeNavModule { }
--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of lhyfe-nav
  */
 
-export * from './lib/lhyfe-nav.service';
-export * from './lib/lhyfe-nav.component';
 export * from './lib/lhyfe-nav.module';
